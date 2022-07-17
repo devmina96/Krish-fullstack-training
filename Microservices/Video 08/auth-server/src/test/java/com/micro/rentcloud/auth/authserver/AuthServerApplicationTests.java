d002dbf4088419dace1a1ffeecbd46d3fe62f0d9
devmina96 <68608303+devmina96@users.noreply.github.com>
2022-07-18 04:57:08 +0530

    Add files via upload

---
+++ Microservices/Video 08/auth-server/src/test/java/com/micro/rentcloud/auth/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.micro.rentcloud.auth.authserver;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
