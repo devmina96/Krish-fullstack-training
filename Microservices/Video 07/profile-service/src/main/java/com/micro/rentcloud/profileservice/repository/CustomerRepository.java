0cab9ae35f10c57cf5d872a532a7653adabf3e39
devmina96 <68608303+devmina96@users.noreply.github.com>
2022-07-20 03:54:29 +0530

    Delete Microservices/Video 07 directory

--- Microservices/Video 07/profile-service/src/main/java/com/micro/rentcloud/profileservice/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.micro.rentcloud.profileservice.repository;
-
-import org.micro.rentcloud.commons.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
