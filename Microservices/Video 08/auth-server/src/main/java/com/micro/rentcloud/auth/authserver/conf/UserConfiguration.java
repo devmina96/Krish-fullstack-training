56bb6a4fc6b23882f182cbe71dd715878675ecb0
devmina96 <68608303+devmina96@users.noreply.github.com>
2022-07-20 03:56:41 +0530

    Add files via upload

---
+++ Microservices/Video 08/auth-server/src/main/java/com/micro/rentcloud/auth/authserver/conf/UserConfiguration.java
@@ -0,0 +1,18 @@
+package com.micro.rentcloud.auth.authserver.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("devmina").password(passwordEncoder.encode("devpass")).roles("USER","ADMIN","MANAGER").authorities("CAN_READ","CAN_WRITE","CAN_DELETE").and()
+                .withUser("shashika").password(passwordEncoder.encode("shashpass")).roles("USER").authorities("CAN_READ","CAN_WRITE");
+    }
+}
