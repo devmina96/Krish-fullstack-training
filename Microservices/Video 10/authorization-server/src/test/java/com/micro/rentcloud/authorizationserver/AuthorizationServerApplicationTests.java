4fa75fc5606fa3fcc18411eba029e95a64e21009
devmina96 <68608303+devmina96@users.noreply.github.com>
2022-07-22 11:32:30 +0530

    Add files via upload

---
+++ Microservices/Video 10/authorization-server/src/test/java/com/micro/rentcloud/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.micro.rentcloud.authorizationserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class AuthorizationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
