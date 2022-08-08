b05a05ef34dec4004e4650a0b8151d2e55f62701
devmina96 <68608303+devmina96@users.noreply.github.com>
2022-08-09 04:22:34 +0530

    Add files via upload

---
+++ Fual-App/fual-distribution-order/src/main/java/com/project/fual/fualdistributionorder/controller/orderController.java
@@ -0,0 +1,29 @@
+package com.project.fual.fualdistributionorder.controller;
+
+import com.project.fual.fualdistributionorder.model.Order;
+import com.project.fual.fualdistributionorder.service.orderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("kafka")
+@CrossOrigin(origins = "http://localhost:4200")
+public class orderController {
+
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String topic = "neworder";
+
+    @RequestMapping(value = "/order", method = RequestMethod.POST)
+    public String post(@RequestBody Order orderData){
+        //Order order1 = orderService.save(order);
+
+        kafkaTemplate.send(topic, orderData.toString());
+        //System.out.println(new Order(orderData.getId(), orderData.getFualType(), orderData.getCapacity()));
+        System.out.println(orderData);
+        return "Success";
+    }
+}
---
+++ Fual-App/fual-distribution-order/src/test/java/com/project/fual/fualdistributionorder/FualDistributionOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fual.fualdistributionorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FualDistributionOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
